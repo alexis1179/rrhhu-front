001401736b865961101d9228ec28f859fe61f53e
ielopezf <irmalopez111@hotmail.es>
2024-09-26 22:41:48 -0600

    Solicitudes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -12,30 +12,49 @@ import "../Styles/Sidebar.css";
 import image from "../image/DSI logo 2.png";
 
 export default function Sidebar() {
-
-    let navigate = useNavigate();
-    let rol = localStorage.getItem("rol") == "ROLE_ADMIN" || localStorage.getItem("rol") == "ROLE_RRHH";
-    const CerrarSesion = () => {
-        console.log("logout");
-        localStorage.clear();
-        navigate("/");
-    }
-    return <>
-        <div className="side-section">
-            <div className="title-navbar">
-                <img src={image} alt="logo" className="logo" />
-                <Logout className="logout" onClick={CerrarSesion} />
-            </div>
-            {rol ?
-                <div className="options">
-                    <Typography onClick={() => navigate("/dashboard")} style={{ cursor: "pointer" }}>Inicio</Typography>
-                    <Typography onClick={() => navigate("/gestionar-usuarios")} style={{ cursor: "pointer" }}>Usuarios</Typography>
-                    <Typography onClick={() => navigate("/asistencia")} style={{ cursor: "pointer" }}>Asistencia</Typography>
-                    <Typography>Nómina</Typography>
-                    <Typography>Reportes</Typography>
-                </div> :
-                <></>}
+  let navigate = useNavigate();
+  let rol =
+    localStorage.getItem("rol") == "ROLE_ADMIN" ||
+    localStorage.getItem("rol") == "ROLE_RRHH";
+  const CerrarSesion = () => {
+    console.log("logout");
+    localStorage.clear();
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="side-section">
+        <div className="title-navbar">
+          <img src={image} alt="logo" className="logo" />
+          <Logout className="logout" onClick={CerrarSesion} />
         </div>
+        {rol ? (
+          <div className="options">
+            <Typography
+              onClick={() => navigate("/dashboard")}
+              style={{ cursor: "pointer" }}
+            >
+              Inicio
+            </Typography>
+            <Typography
+              onClick={() => navigate("/gestionar-usuarios")}
+              style={{ cursor: "pointer" }}
+            >
+              Usuarios
+            </Typography>
+            <Typography
+              onClick={() => navigate("/asistencia")}
+              style={{ cursor: "pointer" }}
+            >
+              Asistencia
+            </Typography>
+            <Typography>Nómina</Typography>
+            <Typography>Reportes</Typography>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 }
