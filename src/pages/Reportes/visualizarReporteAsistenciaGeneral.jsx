8e6cb08ab9ae9cb5785e102f5a1ccab2fbb401f6
ielopezf <irmalopez111@hotmail.es>
2024-11-18 17:38:10 -0600

    cambios general

--- src/pages/Reportes/visualizarReporteAsistenciaGeneral.jsx
+++ src/pages/Reportes/visualizarReporteAsistenciaGeneral.jsx
@@ -10,14 +10,20 @@ import {
     Tooltip,
     Legend,
     ArcElement,
+
 } from 'chart.js';
 import dayjs from 'dayjs';
 import "dayjs/locale/es";
 import url from "../../backUrl";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import { set } from 'date-fns';
 import "../../Styles/HistorialAsistencia.css";
 import Loading from '../../Components/Loading';
 import { useNavigate, useParams } from "react-router-dom";
+import {
+  Button,
+} from "@mui/material";
 
 ChartJS.register(
     CategoryScale,
@@ -50,97 +56,99 @@ const VisualizarReporteAsistenciaGeneral = () => {
     }
 
     // Función para obtener las horas extra y trabajadas en asueto
-    const fetchHoras = async () => {
-        let diurnasVal = 0;
-        let nocturnasVal = 0;
-        let asuetoVal = 0;
-        let diurnasNormalesVal = 0;
-        let diurnas = [];
-        let nocturnas = [];
-        let asueto = [];
-        let diurnasNormales = [];
+const fetchHoras = async () => {
+  let diurnasVal = 0;
+  let nocturnasVal = 0;
+  let asuetoVal = 0;
+  let diurnasNormalesVal = 0;
 
-        const token = localStorage.getItem("token");
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        };
-        try {
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
 
-            try {
-                const resDiurnas = await fetch(`${url}/extras-diurnas/consultar/usuario/${userId}?mes=${mes}&año=${year}`, { headers });
-                diurnas = await resDiurnas.json();
-                diurnasVal = diurnas.find(registro => registro.mes === mesLetras && registro.año === year)?.cantidad_horas || 0;
-                if (diurnas.length > 0) {
-                    setExtraData(prevData => ({
-                        ...prevData,
-                        diurnas: parseFloat(diurnasVal.toFixed(1))
-                    }));
-                }
-            } catch (error) {
-            }
+  try {
+    // Fetch diurnas
+    const resDiurnas = await fetch(`${url}/extras-diurnas`, { headers });
+    const diurnas = await resDiurnas.json();
+    diurnasVal = diurnas
+      .filter((registro) => registro.mes === mesLetras && registro.año === year)
+      .reduce((total, registro) => total + registro.cantidad_horas, 0);
+    setExtraData((prevData) => ({
+      ...prevData,
+      diurnas: parseFloat(diurnasVal.toFixed(1)),
+    }));
 
-            try {
-                const resNocturnas = await fetch(`${url}/extras-nocturnas/consultar/usuario/${userId}?mes=${mes}&año=${year}`, { headers });
-                nocturnas = await resNocturnas.json();
-                nocturnasVal = nocturnas.find(registro => registro.mes === mesLetras && registro.año === year)?.cantidad_horas || 0;
-                if (nocturnas.length > 0) {
-                    setExtraData(prevData => ({
-                        ...prevData,
-                        nocturnas: parseFloat(nocturnasVal.toFixed(1))
-                    }));
-                }
-            } catch (error) {
-            }
+    // Fetch nocturnas
+    const resNocturnas = await fetch(`${url}/extras-nocturnas`, { headers });
+    const nocturnas = await resNocturnas.json();
+    nocturnasVal = nocturnas
+      .filter((registro) => registro.mes === mesLetras && registro.año === year)
+      .reduce((total, registro) => total + registro.cantidad_horas, 0);
+    setExtraData((prevData) => ({
+      ...prevData,
+      nocturnas: parseFloat(nocturnasVal.toFixed(1)),
+    }));
 
-            try {
-                const resAsueto = await fetch(`${url}/asuetos-trabajados/consultar/usuario/${userId}?mes=${mes}&año=${year}`, { headers });
-                asueto = await resAsueto.json();
-                asuetoVal = asueto.find(registro => registro.mes === mesLetras && registro.año === year)?.cantidad_horas || 0;
-                if (asueto.length > 0) {
-                    setExtraData(prevData => ({
-                        ...prevData,
-                        asueto: parseFloat(asuetoVal.toFixed(1))
-                    }));
-                }
-            } catch (error) {
-            }
+    // Fetch asueto
+    const resAsueto = await fetch(`${url}/asuetos-trabajados`, { headers });
+    const asueto = await resAsueto.json();
+    asuetoVal = asueto
+      .filter((registro) => registro.mes === mesLetras && registro.año === year)
+      .reduce((total, registro) => total + registro.cantidad_horas, 0);
+    setExtraData((prevData) => ({
+      ...prevData,
+      asueto: parseFloat(asuetoVal.toFixed(1)),
+    }));
 
-            try {
-                const resDiurnasNormales = await fetch(`${url}/carga-laboral-diurna/consultar/usuario/${userId}?mes=${mes}&año=${year}`, { headers });
-                diurnasNormales = await resDiurnasNormales.json();
-                diurnasNormalesVal = diurnasNormales.find(registro => registro.mes === mesLetras && registro.año === year)?.cantidad_horas || 0;
-                if (diurnasNormales.length > 0) {
-                    setExtraData(prevData => ({
-                        ...prevData,
-                        diurnasNormales: parseFloat(diurnasNormalesVal.toFixed(1))
-                    }));
-                }
-            } catch (error) {
-            }
-            const anio = (diurnas.map(registro => registro.año) + "," + nocturnas.map(registro => registro.año) + "," + asueto.map(registro => registro.año) + "," + diurnasNormales.map(registro => registro.año));
-            const anioUnico = [...new Set(anio.split(","))].sort();
-            setAnios(anioUnico);
-            if (diurnasVal === 0 && nocturnasVal === 0 && asuetoVal === 0 && diurnasNormalesVal === 0) {
-                setData(false);
-            } else {
-                setData(true);
-            }
-            setLoading(false);
+    // Fetch diurnas normales
+    const resDiurnasNormales = await fetch(`${url}/carga-laboral-diurna`, {
+      headers,
+    });
+    const diurnasNormales = await resDiurnasNormales.json();
+    diurnasNormalesVal = diurnasNormales
+      .filter((registro) => registro.mes === mesLetras && registro.año === year)
+      .reduce((total, registro) => total + registro.cantidad_horas, 0);
+    setExtraData((prevData) => ({
+      ...prevData,
+      diurnasNormales: parseFloat(diurnasNormalesVal.toFixed(1)),
+    }));
 
-            if (diurnas.length === 0 && nocturnas.length === 0 && asueto.length === 0 && diurnasNormales.length === 0) {
-                setResultado(false);
-            } else {
-                setResultado(true);
-            }
+    // Set anios
+    const anio =
+      diurnas.map((registro) => registro.año) +
+      "," +
+      nocturnas.map((registro) => registro.año) +
+      "," +
+      asueto.map((registro) => registro.año) +
+      "," +
+      diurnasNormales.map((registro) => registro.año);
+    const anioUnico = [...new Set(anio.split(","))].sort();
+    setAnios(anioUnico);
 
-        } catch (error) {
-            console.error("Error al obtener datos:", error);
-            setData(false);
-            setLoading(false);
-            setResultado(false);
-        }
-    };
+    // Validar que hayan datos
+    setData(
+      diurnasVal > 0 ||
+        nocturnasVal > 0 ||
+        asuetoVal > 0 ||
+        diurnasNormalesVal > 0
+    );
+    setResultado(
+      diurnas.length > 0 ||
+        nocturnas.length > 0 ||
+        asueto.length > 0 ||
+        diurnasNormales.length > 0
+    );
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+    setData(false);
+    setLoading(false);
+    setResultado(false);
+  } finally {
+    setLoading(false);
+  }
+};
 
     useEffect(() => {
         setLoading(true);
@@ -182,69 +190,170 @@ const VisualizarReporteAsistenciaGeneral = () => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
+const generatePDF = async () => {
+  const pdf = new jsPDF();
+
+  // Convertir gráfico a imagen
+  const pieChartElement = document.getElementById("pie-chart");
+  const canvas = await html2canvas(pieChartElement);
+  const imgData = canvas.toDataURL("image/png");
+  // setear tamaño de la imagen
+  const imgWidth = 100;
+  const imgHeight = 100;
+  // centrar la imagen en la pagina
+const pdfWidth = pdf.internal.pageSize.getWidth();// ancho de la pagina
+  const xPosition = (pdfWidth - imgWidth) / 2;
+  // Agregamos imagen al documento
+ pdf.addImage(imgData, "PNG", xPosition, 10, imgWidth, imgHeight);
+
+  // Agregar titulo al documento
+  pdf.setFontSize(20);
+  pdf.text(
+    `Reporte de asistencia - ${capitalizeFirstLetter(
+      dayjs()
+        .locale("es")
+        .month(mes - 1)
+        .format("MMMM")
+    )} ${displayYear}`,
+    xPosition,
+    imgHeight + 20
+  );
+
+  // Crear tabla
+  pdf.setFontSize(12);
+  const tableData = [
+    ["Tipo de Horas", "Cantidad (horas)"],
+    ["Horas Diurnas", extraData.diurnas],
+    ["Horas Nocturnas", extraData.nocturnas],
+    ["Horas en Asueto", extraData.asueto],
+    ["Horas Diurnas Normales", extraData.diurnasNormales],
+  ];
+  // posicionar la tabla
+let startY = imgHeight + 30;
+  tableData.forEach((row, index) => {
+    const y = startY + index * 10;
+    pdf.text(row[0], 10, y);
+    pdf.text(row[1].toString(), 100, y);
+  });
+
+  //generar pdf
+  pdf.save(`reporte_asistencia_${displayYear}_${mesLetras}.pdf`);
+};
+
     return (
-        <>
-            <Sidebar />
-            <div style={{ textAlign: 'center', margin: '20px' }}>
-                <h1>Reporte de asistencia general - {capitalizeFirstLetter(dayjs().locale('es').month(mes - 1).format('MMMM'))} {displayYear}</h1>
-                {resultados ?
-                    <div className='filters'>
-                        <label>Seleccionar año: </label>
-                        <select value={year} onChange={(e) => setYear(e.target.value)}>
-                            {anios.map((anio) => (
-                                <option key={anio} value={anio}>
-                                    {anio}
-                                </option>
-                            ))}
-                        </select>
-                        <label>Seleccionar mes: </label>
-                        <select value={mes} onChange={handleMonthChange}>
-                            {Array.from({ length: 12 }, (_, i) => (
-                                <option key={i} value={i + 1}>
-                                    {capitalizeFirstLetter(dayjs().locale('es').month(i).format('MMMM'))}  {/* Capitaliza manualmente */}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    : <></>}
-                {loading ?
-                    <Loading /> : (
-                        <>
-                            {data ? (
-                                <div>
-                                    <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-                                        <div style={{ width: '40%', margin: '10px' }}>
-                                            <Pie data={pieData} options={options} />
-                                        </div>
-                                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                            <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-                                                <div style={{ width: '25vw', margin: '10px' }}>
-                                                    <h2>Horas Diurnas</h2>
-                                                    <p>{extraData.diurnas} horas</p>
-                                                </div>
-                                                <div style={{ width: '50%', margin: '10px' }}>
-                                                    <h2>Horas Nocturnas</h2>
-                                                    <p>{extraData.nocturnas} horas</p>
-                                                </div>
-                                            </div>
-                                            <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-                                                <div style={{ width: '50%', margin: '10px' }}>
-                                                    <h2>Horas en asueto</h2>
-                                                    <p>{extraData.asueto} horas</p>
-                                                </div>
-                                                <div style={{ width: '50%', margin: '10px' }}>
-                                                    <h2>Horas Diurnas Normales</h2>
-                                                    <p>{extraData.diurnasNormales} horas</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ) : <p>No hay registros</p>}
-                        </>
-                    )}
+      <>
+        <Sidebar />
+        <div style={{ textAlign: "center", margin: "20px" }}>
+          <h1>
+            Reporte de asistencia general -{" "}
+            {capitalizeFirstLetter(
+              dayjs()
+                .locale("es")
+                .month(mes - 1)
+                .format("MMMM")
+            )}{" "}
+            {displayYear}
+          </h1>
+          {resultados ? (
+            <div className="filters">
+              <label>Seleccionar año: </label>
+              <select value={year} onChange={(e) => setYear(e.target.value)}>
+                {anios.map((anio) => (
+                  <option key={anio} value={anio}>
+                    {anio}
+                  </option>
+                ))}
+              </select>
+              <label>Seleccionar mes: </label>
+              <select value={mes} onChange={handleMonthChange}>
+                {Array.from({ length: 12 }, (_, i) => (
+                  <option key={i} value={i + 1}>
+                    {capitalizeFirstLetter(
+                      dayjs().locale("es").month(i).format("MMMM")
+                    )}{" "}
+                    {/* Capitaliza manualmente */}
+                  </option>
+                ))}
+              </select>
             </div>
-        </>
+          ) : (
+            <></>
+          )}
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              {data ? (
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      margin: "20px",
+                    }}
+                  >
+                    <div
+                      id="pie-chart"
+                      style={{ width: "40%", margin: "10px" }}
+                    >
+                      <Pie data={pieData} options={options} />
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          margin: "20px",
+                        }}
+                      >
+                        <div style={{ width: "25vw", margin: "10px" }}>
+                          <h2>Horas Diurnas</h2>
+                          <p>{extraData.diurnas} horas</p>
+                        </div>
+                        <div style={{ width: "50%", margin: "10px" }}>
+                          <h2>Horas Nocturnas</h2>
+                          <p>{extraData.nocturnas} horas</p>
+                        </div>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          margin: "20px",
+                        }}
+                      >
+                        <div style={{ width: "50%", margin: "10px" }}>
+                          <h2>Horas en asueto</h2>
+                          <p>{extraData.asueto} horas</p>
+                        </div>
+                        <div style={{ width: "50%", margin: "10px" }}>
+                          <h2>Horas Diurnas Normales</h2>
+                          <p>{extraData.diurnasNormales} horas</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <p>No hay registros</p>
+              )}
+            </>
+          )}
+        </div>
+        {!loading && data && ( // validar si existe data para el mes y anio seleccionado
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "10px 0",
+            }}
+          >
+            <Button variant="contained" color="primary" onClick={generatePDF}>
+              Exportar a PDF
+            </Button>
+          </div>
+        )}
+      </>
     );
 };
 
